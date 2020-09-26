e1db0b86b4b89f367f026a44ed90be8daa182ccb
Rowan-James Tran <rowanjames.tran@gmail.com>
2020-09-25 23:55:43 -0700

    Style team page

--- src/templates/team.js
+++ src/templates/team.js
@@ -1,9 +1,24 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { styled } from "linaria/react";
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  max-width: 1200px;
+  padding: 40px;
+  border-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.5);
+`;
 
 function Team({ data }) {
   return (
-    <div dangerouslySetInnerHTML={{__html: data.file.childMarkdownRemark.html}} />
+    <Wrapper>
+      <div dangerouslySetInnerHTML={{__html: data.file.childMarkdownRemark.html}} />
+    </Wrapper>
   );
 }
 
