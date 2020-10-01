61c1dbb894c6be433e0e4e74c1a8b4c0c6f33736
Rowan-James Tran <rowanjames.tran@gmail.com>
2020-10-01 02:06:23 -0700

    Use hooks to calculate threshold

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import useScrollPosition from '@react-hook/window-scroll';
 
 import Layout from "../components/layout"
@@ -14,18 +14,24 @@ import FAQ from "../components/faq";
 import Contact from "../components/contact";
 
 const IndexPage = () => {
+  const [sticking, setSticking] = useState(false);
   const scrollY = useScrollPosition(30);
-  console.log(scrollY);
-  let scrollThreshold = 200; // Height of top header
+  const [scrollThreshold, setScrollThreshold] = useState(200);
 
-  if (window.innerWidth <= 600) {
-    // Match change in header subtext to 2rem
-    scrollThreshold = (0.11 * window.innerWidth) + 91;
-  } else if (window.innerWidth <= 768)  {
-    scrollThreshold = (0.11 * window.innerWidth) + 112;
-  }
+  useEffect(() => {
+    if (window.innerWidth <= 600) {
+      // Match change in header subtext to 2rem
+      setScrollThreshold((0.11 * window.innerWidth) + 91);
+    } else if (window.innerWidth <= 768)  {
+      setScrollThreshold((0.11 * window.innerWidth) + 112);
+    } else {
+      setScrollThreshold(200);
+    }
+  }, [window.innerWidth])
 
-  let sticking = (scrollY > scrollThreshold);
+  useEffect(() => {
+    setSticking(scrollY > scrollThreshold);
+  }, [scrollY]);
 
   return (
     <>
